477e4c85a01cb94830c0a851727226dc48aa436f
Jason McCallister <jason@mccallister.dev>
2025-06-20 10:29:29 -0400

    working wip
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,48 @@
 import * as vscode from 'vscode';
-import DaggerCommands from './commands/commands';
+import DaggerCli from './cli/cli';
 
 export function activate(context: vscode.ExtensionContext) {
-	const commands = new DaggerCommands(context);
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders || workspaceFolders.length === 0) {
+        throw new Error('No workspace folder is open. Please open a folder or workspace first.');
+    }
+    const workspace = workspaceFolders[0].uri;
+
+	const cli = new DaggerCli('dagger', workspace);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('dagger.init', async () => {
+			// check if this workspace is already a dagger project
+			if (await cli.isDaggerProject()) {
+				// show an error message if it is and prompt the user to run the develop command
+				
+
+			
+				return;
+			}
+
+			await cli.run(['init']);
+		}),
+		vscode.commands.registerCommand('dagger.develop', async () => {
+			// check if this workspace is already a dagger project
+			if (!await cli.isDaggerProject()) {
+				// show an error message if it is and prompt the user to run the init command
+				vscode.window.showErrorMessage('This workspace is not a Dagger project. Please run the "Dagger: Init" command to initialize it.');
+				return;
+			}
+
+			await cli.run(['develop']);
+		}),
+		vscode.commands.registerCommand('dagger.functions', async () => {
+			// check if this workspace is already a dagger project
+			if (!await cli.isDaggerProject()) {
+				// show an error message if it is and prompt the user to run the init command
+				vscode.window.showErrorMessage('This workspace is not a Dagger project. Please run the "Dagger: Init" command to initialize it.');
+				return;
+			}
+
+			await cli.run(['functions']);
+		})
+	);
 }
 
 export function deactivate() {}
