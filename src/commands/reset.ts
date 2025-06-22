a008f326619950885a271ca140e4032479be0f78
Jason McCallister <jason@mccallister.dev>
2025-06-21 23:51:36 -0400

    delete from secret if it exists
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- src/commands/reset.ts
+++ src/commands/reset.ts
@@ -4,13 +4,6 @@ export default function resetCommand(context: vscode.ExtensionContext) {
     context.subscriptions.push(
         vscode.commands.registerCommand('dagger.resetPreferences', async () => {
             const config = vscode.workspace.getConfiguration('dagger');
-            const currentToken = config.get<string>('cloudToken', '');
-            const envToken = process.env.DAGGER_CLOUD_TOKEN;
-
-            if (!currentToken && !envToken) {
-                vscode.window.showInformationMessage('No Dagger Cloud token is set. No action taken.');
-                return;
-            }
 
             const response = await vscode.window.showWarningMessage(
                 'Are you sure you want to reset your Dagger preferences?',
@@ -20,7 +13,8 @@ export default function resetCommand(context: vscode.ExtensionContext) {
             );
 
             if (response === 'Yes') {
-                await config.update('cloudToken', '', vscode.ConfigurationTarget.Global);
+                await context.secrets.delete('dagger.cloudToken');
+                await config.update('cloudNotificationDismissed', false, vscode.ConfigurationTarget.Global);
                 vscode.window.showInformationMessage('Dagger preferences have been reset.');
             }
         })
