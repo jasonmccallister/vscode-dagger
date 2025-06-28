4a5f4123be4038ba9fae6e61fead68cdb9924cb5
Jason McCallister <jason@mccallister.dev>
2025-06-28 13:40:14 -0600

    cleaning up
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

---
+++ src/test/tree/tree-provider.test.ts
@@ -0,0 +1,41 @@
+import * as assert from 'assert';
+import { describe, it, beforeEach } from 'mocha';
+import { DataProvider, Item } from '../../tree/provider';
+import Cli, { FunctionInfo } from '../../dagger/dagger';
+import sinon from 'sinon';
+
+describe('Tree Provider', () => {
+    let mockCli: Partial<Cli>;
+    let dataProvider: DataProvider;
+
+    beforeEach(() => {
+        mockCli = {
+            isInstalled: async () => true,
+            isDaggerProject: async () => true,
+            functionsList: async () => [],
+            getFunctionArguments: async () => []
+        };
+    });
+
+    it('should load data with test items on construction', async () => {
+        // Arrange: mock functionsList to return test items
+        const testFunctions: FunctionInfo[] = [
+            { name: 'testFunc1', description: 'desc1' },
+            { name: 'testFunc2', description: 'desc2' }
+        ];
+        mockCli.functionsList = async () => testFunctions;
+        mockCli.isInstalled = async () => true;
+        mockCli.isDaggerProject = async () => true;
+        
+        // Act
+        dataProvider = new DataProvider(mockCli as Cli, '');
+        // Wait for async loadData to finish
+        await new Promise(resolve => setTimeout(resolve, 10));
+        const children = await dataProvider.getChildren();
+        
+        // Assert
+        assert.strictEqual(children.length, 2);
+        assert.strictEqual(children[0].label, 'testFunc1');
+        assert.strictEqual(children[1].label, 'testFunc2');
+    });
+});
